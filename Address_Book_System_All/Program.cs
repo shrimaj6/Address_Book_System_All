a76b8bd63227a479b552e720f99982fa1acfeba6
Shrimaj <shrimaj.mehta60@gmail.com>
2021-12-09 17:14:40 +0530

    Add project files.

---
+++ Address_Book_System_All/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_System_All
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program.");
+        }
+    }
+}
